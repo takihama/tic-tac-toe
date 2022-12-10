38ce9f60ab56c7f934ceae80de718e6283ed118e
Daniel <dacalderon@thefork.com>
2022-12-10 12:56:51 -0300

    initial client commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  Stack, Text } from '@chakra-ui/react';
+
+const App = () => {
+  return (
+    <Stack>
+      <Text>Tic-Tac-Toe</Text>    
+    </Stack>
+  );
+};
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>  
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
