9000ee0103cd9b37ae708ae4020e134deda91ef0
Daniel <dacalderon@thefork.com>
2022-12-10 21:12:48 -0300

    feat: add field

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import {  Stack, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Button, Container, Stack, Text } from '@chakra-ui/react';
+
+const INITIAL_FIELD = [
+  ['x', '', ''],
+  ['', 'o', 'o'],
+  ['', 'x', ''],
+];
+
+const Square = ({ value, handleOnSquareClick }) => {
+  return (
+    <Button bg="purple.500" w="100px" h="100px" onClick={handleOnSquareClick}>
+      <Text fontSize="6xl">{value}</Text>
+    </Button>
+  );
+};
 
 const App = () => {
+  const [field, setField] = useState(INITIAL_FIELD);
+
   return (
-    <Stack>
-      <Text>Tic-Tac-Toe</Text>    
-    </Stack>
+    <Container bg="primary.300" h="100vh">
+        <Stack h="full" align="center" justify="center">
+          {field.map((row, rowIdx) => (
+            <Stack key={rowIdx} direction="row">
+              {row.map((cell, colIdx) => (
+                <Square key={colIdx} value={cell} />
+              ))}
+            </Stack>
+          ))}
+        </Stack>
+    </Container>
   );
 };
 
