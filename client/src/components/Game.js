3cd00e4c5ae35eae43897d616c344f6886a18107
Daniel <dacalderon@thefork.com>
2022-12-11 18:11:12 -0300

    feat: add singleplayer and twoplayer game modes

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,91 +1,155 @@
 import React, { useEffect, useState } from 'react';
-import { Heading, Stack } from '@chakra-ui/react';
+import { Heading, Radio, RadioGroup, Stack } from '@chakra-ui/react';
 import Field from './Field';
 import StartRestartButton from './StartRestartButton';
 
 import { checkWinner } from '../utils/checkWinner';
-import minimax from '../utils/minimax';
-import { getOpponent, SYMBOLS } from '../utils/getOpponent';
-import { getRandomPlayer } from '../utils/getRandomPlayer';
+import { minimax } from '../utils/minimax';
 
-const INITIAL_FIELD = [
+const INITIAL_BOARD = [
   ['', '', ''],
   ['', '', ''],
   ['', '', ''],
 ];
 
+const getActivePlayer = players => players.find(player => player.active);
+
 const Game = () => {
-  const [hasGameStarted, setHasGameStarted] = useState(false);
-  const [field, setField] = useState(INITIAL_FIELD);
-  const [turn, setTurn] = useState();
+  const [gameBoard, setGameBoard] = useState(INITIAL_BOARD);
+  const [gameMode, setGameMode] = useState('singlePlayer');
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameFinished, setGameFinished] = useState(false);
   const [gameResult, setGameResult] = useState(null);
 
+  const [players, setPlayers] = useState();
+
   const onStartGame = () => {
-    const randPlayer = getRandomPlayer();
-    setHasGameStarted(true);
-    setField(INITIAL_FIELD);
-    setTurn(randPlayer);
+    switch (gameMode) {
+      case 'singlePlayer': {
+        setPlayers([
+          {
+            symbol: 'x',
+            type: 'human',
+            active: true,
+          },
+          {
+            symbol: 'o',
+            type: 'computer',
+            active: false,
+          },
+        ]);
+        break;
+      }
+      case 'twoPlayers': {
+        setPlayers([
+          {
+            symbol: 'x',
+            type: 'human',
+            active: true,
+          },
+          {
+            symbol: 'o',
+            type: 'human',
+            active: false,
+          },
+        ]);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+    setGameBoard(INITIAL_BOARD);
+    setGameStarted(true);
+    setGameFinished(false);
     setGameResult(null);
   };
 
-  const onSquareClick = (x, y) => {
-    // if square has already a value, return
-    if (field[y][x]) return;
-
-    setField(prevField =>
-      prevField.map((row, rowIdx) =>
-        rowIdx !== y
-          ? row
-          : row.map((col, colIdx) => (colIdx !== x ? col : turn))
-      )
-    );
-
-    setTurn(prevState => getOpponent(prevState));
+  const onChangeGameMode = mode => {
+    setGameMode(mode);
   };
 
-  useEffect(() => {
-    // if game not started, no need to check for game result
-    if (!hasGameStarted) return;
-
-    const result = checkWinner(field, turn);
-    if (result) {
-      setGameResult(result);
-    }
-  }, [hasGameStarted, field]);
-
-  useEffect(() => {
-    if (!hasGameStarted || gameResult || turn !== SYMBOLS.O) return;
+  const playMove = (x, y) => {
+    if (!gameStarted || gameFinished) return;
     
-    const { move } = minimax(field, true, turn);
-    if (move === null) return;
+    // if square has already a value, return
+    if (gameBoard[y][x]) return;
 
-    setField(
-      field.map((row, rowIdx) =>
-        rowIdx !== move[0]
-          ? row
-          : row.map((col, colIdx) => (colIdx !== move[1] ? col : turn))
-      )
+    const turnPlayer = getActivePlayer(players);
+
+    const newGameBoard = gameBoard.map((row, rowIdx) =>
+      rowIdx !== y
+        ? row
+        : row.map((col, colIdx) => (colIdx !== x ? col : turnPlayer.symbol))
     );
 
-    setTurn(prevState => getOpponent(prevState));
-  }, [field, turn]);
+    const result = checkWinner(newGameBoard, turnPlayer.symbol);
+    if (result) {
+      setGameFinished(true);
+      setGameResult(result);
+    }
+
+    setGameBoard(newGameBoard);
+  };
+
+  const onSquareClick = (x, y) => {
+    playMove(x, y);
+  };
+
+  // when a player plays a move -> updates turn
+  useEffect(() => {
+    if (!gameStarted || gameFinished) return;
+    const changeTurn = players =>
+      players.map(player => ({
+        ...player,
+        active: !player.active,
+      }));
+
+    setPlayers(prevState => changeTurn(prevState));
+  }, [gameBoard]);
+
+  // when the turn changes -> computer plays?
+  useEffect(() => {
+    if (!gameStarted && !gameFinished) return;
+
+    const turnPlayer = getActivePlayer(players);
+
+    if (turnPlayer.type === 'computer') {
+      const { move } = minimax(gameBoard, true, turnPlayer.symbol);
+
+      playMove(move[1], move[0]);
+    }
+  }, [players]);
 
   return (
     <Stack h="full" align="center" justify="center">
-      {gameResult && (
+      {!gameStarted && (
+        <>
+          <StartRestartButton value="Start" handleOnClick={onStartGame} />
+          <RadioGroup onChange={onChangeGameMode} value={gameMode}>
+            <Stack direction="row">
+              <Radio value="singlePlayer">Single player</Radio>
+              <Radio value="twoPlayers">2 players</Radio>
+            </Stack>
+          </RadioGroup>
+        </>
+      )}
+
+      {gameFinished && (
         <Stack align="center" justify="center">
           <Heading fontSize="4xl">
+            {gameResult !== 'tie' ? getActivePlayer(players).symbol : ''}{' '}
             {gameResult}
           </Heading>
         </Stack>
       )}
-      {!hasGameStarted && (
-        <StartRestartButton value="Start" handleOnClick={onStartGame} />
-      )}
-      {hasGameStarted && (
+
+      {gameStarted && (
         <>
-          <Heading fontSize="4xl">{turn}'s turn</Heading>
-          <Field field={field} handleOnSquareClick={onSquareClick} />
+          <Heading fontSize="4xl">
+            {/* {getActivePlayer(players).symbol}'s turn */}
+          </Heading>
+          <Field field={gameBoard} handleOnSquareClick={onSquareClick} />
           <StartRestartButton value="Restart" handleOnClick={onStartGame} />
         </>
       )}
